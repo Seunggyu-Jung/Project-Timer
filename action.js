eb76423bd8797b4a5ccd653d3faba20e9e83067b
ThovenhairWorrior <wswm2007@naver.com>
2023-05-12 14:24:27 +0900

    바뀌어야할 요소 js로 지정

---
+++ action.js
@@ -0,0 +1,5 @@
+const startDefault = document.querySelector(".start");
+const stopDefault= document.querySelector(".stop");
+const numHour = document.querySelector(".component.hour");
+const numMinute = document.querySelector(".component.minute");
+const numSecond = document.querySelector(".component.second");
